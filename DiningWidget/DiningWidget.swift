02cec327c9c3c3db228252f80eab1ff29ae37a5a
Lonnie Gerol <lonnie@lonniegerol.com>
2020-12-24 16:07:20 -0500

    Created First Widget Design

--- DiningWidget/DiningWidget.swift
+++ DiningWidget/DiningWidget.swift
@@ -40,13 +40,53 @@ struct SimpleEntry: TimelineEntry {
     let configuration: ConfigurationIntent
 }
 
-struct DiningWidgetEntryView : View {
+struct DiningWidgetEntryView: View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        VStack(alignment: .leading) {
+            // Location
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("GV Cantina and Grill")
+                        .font(.largeTitle)
+                        .fontWeight(.medium)
+                        .lineLimit(3)
+                        .minimumScaleFactor(0.30)
+                        //.scaledToFit()
+                        .foregroundColor(.white)
+                        //.padding(5)
+                    Spacer()
+                    Circle()
+                        .foregroundColor(Color.green)
+                        .frame(width: 25, height: 25, alignment: .center)
+                }
+            }
+            .padding()
+            Spacer()
+            // Hours
+            VStack(alignment: .leading, content: {
+                Text("8:00pm - 9:00pm")
+                    //.lineLimit(1)
+                    //.foregroundColor(Color.white)
+                Spacer()
+                    .frame(height: 5, alignment: .center)
+                Text("12:00pm - 12:00pm")
+            })
+            .lineLimit(1)
+            //.font(.system(size: 13))
+            .minimumScaleFactor(0.30)
+            //fixedSize(horizontal: true, vertical: false)
+            .foregroundColor(Color.white)
+            .padding(10)
+
+            Spacer()
+
+        }
+        .background(Color.init("RIT Orange"))
+
+        }
     }
-}
 
 @main
 struct DiningWidget: Widget {
@@ -56,8 +96,8 @@ struct DiningWidget: Widget {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             DiningWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Single Location")
+        .description("A Widget that displays the hours of operation for a single location.")
     }
 }
 
@@ -65,5 +105,7 @@ struct DiningWidget_Previews: PreviewProvider {
     static var previews: some View {
         DiningWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+
     }
 }
